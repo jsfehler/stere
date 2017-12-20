174e7cf681f0c8b79f3046a40dbca9822d0255fb
Joshua Fehler <jsfehler@gmail.com>
2017-12-19 21:57:45 -0500

    Don't expose splinter strategy classes

--- stere/strategy/__init__.py
+++ stere/strategy/__init__.py
@@ -1,10 +1,2 @@
 from .strategy import strategy
 from .strategy import strategies
-
-from .splinter_strategies import FindByCss
-from .splinter_strategies import FindByXPath
-from .splinter_strategies import FindByTag
-from .splinter_strategies import FindByName
-from .splinter_strategies import FindByText
-from .splinter_strategies import FindById
-from .splinter_strategies import FindByValue
