5734ea7dab59df2ee67aac95d3ff710bdae19e6e
Joshua Fehler <jsfehler@gmail.com>
2017-12-19 19:53:42 -0500

    Start adding unit tests

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,15 +1,64 @@
+import time
+
+
 from stere import Stere, Goto
-from stere.fields import Input
+from stere.fields import Area, Button, Input
 from pages import google
 
 
-def test_input(browser):
+class DummyPage():
+    """A page object for the test page."""
+    
+    def __init__(self):
+        self.button = Button('id', 'test_button')
+        self.area = Area(
+            input = Input('id', 'test_input'),
+            submit_button = Button('id', 'test_input_submit')
+        )
+
+
+def test_button(browser):
+    """
+    When a button is clicked
+    Then the button's action occurs
+    """
     Stere.browser = browser
 
-    google_input = Input('xpath', '//*[@id="lst-ib"]')
+    browser.visit('https://jsfehler.github.io/stere/test_page/test_page.html')
+    
+    test_page = DummyPage()
+    test_page.button.click()
+    
+    # Clicking the button turns the button's container background colour
+    assert 'red' == test_page.button.element.style.background_color
 
-    browser.visit('http://google.ca')
-    google_input.fill('Winamp')
+
+def test_input(browser):
+    """
+    When an input is filled with the text 'Winamp'
+    Then the text in the input should be 'Winamp'
+    """
+    Stere.browser = browser
+
+    browser.visit('https://jsfehler.github.io/stere/test_page/test_page.html')
+    
+    test_page = DummyPage()
+    test_page.area.input.fill('Winamp')
+    
+    assert 'Winamp' == test_page.area.input.element.value
+
+
+def test_area_perform(browser):
+    Stere.browser = browser
+
+    browser.visit('https://jsfehler.github.io/stere/test_page/test_page.html')
+    
+    test_page = DummyPage()
+    test_page.area.perform('Winamp')
+
+    time.sleep(2)
+    
+    assert 'https://google.ca' == browser.url
 
 
 def test_stere(browser):
