429155752533840fd6f5e83ac1c25e5c021f9b88
Joshua Fehler <jsfehler@gmail.com>
2017-12-26 20:09:20 -0500

    Re-expose FindBy classes

--- stere/strategy/__init__.py
+++ stere/strategy/__init__.py
@@ -1,2 +1,10 @@
 from .strategy import strategy  # NOQA: F401
 from .strategy import strategies  # NOQA: F401
+
+from .splinter_strategies import FindByCss  # NOQA: F401
+from .splinter_strategies import FindByXPath  # NOQA: F401
+from .splinter_strategies import FindByTag  # NOQA: F401
+from .splinter_strategies import FindByName  # NOQA: F401
+from .splinter_strategies import FindByText  # NOQA: F401
+from .splinter_strategies import FindById  # NOQA: F401
+from .splinter_strategies import FindByValue  # NOQA: F401
