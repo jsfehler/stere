89cc53867b2e0b93e99b3e35f09d56ee38f19a78
Joshua Fehler <jsfehler@gmail.com>
2018-02-07 18:42:27 -0500

    Tests: Move splinter strategy tests to separate file

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -126,23 +126,3 @@ def test_repeating_area(browser):
     listings = test_page.repeating_area.areas
     assert listings[0].link.text == "Repeating Link 1"
     assert listings[1].link.text == "Repeating Link 2"
-
-
-def test_is_present_args():
-    """
-    When I send an argument to is_present
-    Then it is used by the correct function
-    """
-    test_page = dummy.DummyPage()
-    test_page.visit()
-    assert test_page.added_container.is_present(wait_time=6)
-
-
-def test_is_not_present_args():
-    """
-    When I send an argument to is_not_present
-    Then it is used by the correct function
-    """
-    test_page = dummy.DummyPage()
-    test_page.visit()
-    assert test_page.removed_container.is_not_present(wait_time=6)
