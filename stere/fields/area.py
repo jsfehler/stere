70b3b0d198f5e02388f9aa4aa6c0d6b0c30116c8
Joshua Fehler <jsfehler@gmail.com>
2018-02-10 13:49:05 -0500

    fixup

--- stere/fields/area.py
+++ stere/fields/area.py
@@ -26,12 +26,17 @@ class Area():
         """For every Field in an Area, sequentially "do the right thing"
         by calling the Field's perform() method.
 
+        Every Field that implements perform() must return True or False.
+        If True, assume the argument has been used. If False,
+        assume the argument is still available.
+
         Args:
             args: Array that should be equal to the number of
                 Fields in the Area that take an argument.
         """
         arg_index = 0
-        for key, value in self.items.items():
-            result = value.perform(args[arg_index])
-            if result:
+        for field_name, field in self.items.items():
+            result = field.perform(args[arg_index])
+            # If we've run out of arguments, don't increase the index.
+            if result and len(args) < arg_index:
                 arg_index += 1
