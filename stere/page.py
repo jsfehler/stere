e46208cad90d4912f39abfb68707551fd5c1ff4b
Joshua Fehler <jsfehler@gmail.com>
2018-02-18 16:07:15 -0500

    Remove Page.scroll_to method. It's outside the scope of the project

--- stere/page.py
+++ stere/page.py
@@ -17,7 +17,3 @@ class Page(BrowserEnabled):
         """Opens the page in the browser, based on the url attribute.
         """
         self.browser.visit(self.url)
-
-    def scroll_to(self, x, y):
-        """Scroll to a specific location on the page."""
-        self.browser.driver.execute_script(f'window.scrollTo({x}, {y})')
