b23bee09496e1cb4393083a9c56c701dd381d06c
Joshua Fehler <jsfehler@gmail.com>
2018-04-26 00:05:34 -0400

    Add RepeatingArea.area_with() - Update tests with fixture for test_page

--- tests/test_splinter_strategies.py
+++ tests/test_splinter_strategies.py
@@ -2,26 +2,22 @@ import logging
 
 from selenium.webdriver.remote.remote_connection import LOGGER
 
-from pages import dummy
-
 LOGGER.setLevel(logging.WARNING)
 
 
-def test_is_present_args():
+def test_is_present_args(test_page):
     """
     When I send an argument to is_present
     Then it is used by the correct function
     """
-    test_page = dummy.DummyPage()
     test_page.visit()
     assert test_page.added_container.is_present(wait_time=6)
 
 
-def test_is_not_present_args():
+def test_is_not_present_args(test_page):
     """
     When I send an argument to is_not_present
     Then it is used by the correct function
     """
-    test_page = dummy.DummyPage()
     test_page.visit()
     assert test_page.removed_container.is_not_present(wait_time=6)
