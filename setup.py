b2542274b89c329fae37a15ab0d5a0100a83bfb9
Joshua Fehler <jsfehler@users.noreply.github.com>
2018-07-05 15:49:19 -0400

    Add short description in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="stere",
     version="0.0.1",
+    description="A nice way of implementing the Page Object pattern.",
     author="Joshua Fehler",
     author_email="jsfehler@gmail.com",
     license="MIT",
