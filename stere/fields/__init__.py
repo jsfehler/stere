2ceb1e0699b869f159efa3fcd8a460bd3010ccd8
Joshua Fehler <jsfehler@gmail.com>
2018-07-08 21:40:23 -0400

    Fixup

--- stere/fields/__init__.py
+++ stere/fields/__init__.py
@@ -1,6 +1,7 @@
 from .field import Field  # NOQA: F401
 
 from .splinter_fields import Button  # NOQA: F401
+from .splinter_fields import Checkbox  # NOQA: F401
 from .splinter_fields import Input  # NOQA: F401
 from .splinter_fields import Link  # NOQA: F401
 from .splinter_fields import Root  # NOQA: F401
