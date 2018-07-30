e2252a40e36331284150cfbf9732fec098606274
Joshua Fehler <jsfehler@gmail.com>
2018-07-30 14:59:52 -0400

    Fix incorrect test

--- tests/test_splinter_strategies.py
+++ tests/test_splinter_strategies.py
@@ -24,12 +24,12 @@ def test_is_not_present_args(test_page):
     assert test_page.removed_container.is_not_present(wait_time=6)
 
 
-def test_button_alt_strategy(browser, test_page):
+def test_button_data_star_strategy(browser, test_page):
     """
     When I define a Field using a data-* strategy, it is found.
     """
     test_page.navigate()
-    test_page.button.click()
+    test_page.button_alt_strategy.click()
 
     # Clicking changes the button's container background colour
     browsers = {
@@ -42,3 +42,8 @@ def test_button_alt_strategy(browser, test_page):
     actual = test_page.button_container.first._element.value_of_css_property(
         'background-color')
     assert browsers[os.environ["CURRENT_BROWSER_NAME"]] == actual
+
+
+def test_data_star_staregy_is_present(browser, test_page):
+    test_page.navigate()
+    assert test_page.button_alt_strategy.is_present(wait_time=6)
