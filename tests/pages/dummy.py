f0df5d1cc72c9187f1d1f429a66e3ed56cd7d2d1
Joshua Fehler <jsfehler@gmail.com>
2018-08-01 20:24:51 -0400

    Tests: FindByDataStarAttribute.is_not_present()

--- tests/pages/dummy.py
+++ tests/pages/dummy.py
@@ -30,6 +30,9 @@ class DummyPage(Page):
         self.button_container = Root('id', 'test_button_div')
         self.button = Button('id', 'test_button')
 
+        # Field for something that isn't on the page.
+        self.missing_button = Button('data-test-id', 'not_on_the_page')
+
         # Targets the same button as above, but using a different strategy.
         self.button_alt_strategy = Button('data-test-id', 'Stere_test_button')
 
