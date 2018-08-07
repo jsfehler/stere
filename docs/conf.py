c4670ab00523445cd14757af2834cea0b1897c6e
Joshua Fehler <jsfehler@gmail.com>
2018-08-07 13:48:58 -0400

    Bump version to 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ author = 'Joshua Fehler'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.0'
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.1.0'
 
