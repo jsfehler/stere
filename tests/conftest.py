e6a0d2271d8e94d37a772f1e2b47ac795609eec1
Joshua Fehler <jsfehler@gmail.com>
2018-08-07 13:00:09 -0400

    fixup 4

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,7 @@ def pytest_runtest_makereport(item, call):
     setattr(item, "rep_" + rep.when, rep)
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def after(request, browser):
 
     def fin():
