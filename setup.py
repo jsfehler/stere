fca4118d934f2e5e4a295c254a1aaf0c37c26e84
Joshua Fehler <jsfehler@gmail.com>
2018-08-07 13:46:47 -0400

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+def read(filename):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    with open(path, 'r') as f:
+        return f.read()
+
 
 setup(
     name="stere",
     version="0.1.0",
     description="A nice way of implementing the Page Object pattern.",
+    long_description=read('README.rst'),
     author="Joshua Fehler",
     author_email="jsfehler@gmail.com",
     license="MIT",
