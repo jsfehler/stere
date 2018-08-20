f8503fdc5cc3bca598ded796e0e93abdc8901ac1
Joshua Fehler <jsfehler@users.noreply.github.com>
2018-08-20 11:37:21 -0400

    Add is_visible and is_not_visible methods to splinter strategies (#57)

--- tests/test_splinter_strategies.py
+++ tests/test_splinter_strategies.py
@@ -6,6 +6,16 @@ from selenium.webdriver.remote.remote_connection import LOGGER
 LOGGER.setLevel(logging.WARNING)
 
 
+def test_is_visible(test_page):
+    test_page.navigate()
+    assert test_page.added_container.is_visible(wait_time=6)
+
+
+def test_is_not_visible(test_page):
+    test_page.navigate()
+    assert test_page.removed_container.is_not_visible(wait_time=6)
+
+
 def test_is_present_args(test_page):
     """
     When I send an argument to is_present
