468abe5efb1978b5ac571769a76e610cd0a15675
Joshua Fehler <jsfehler@users.noreply.github.com>
2018-10-16 23:41:08 -0400

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(filename):
 
 setup(
     name="stere",
-    version="0.2.1",
+    version="0.2.2",
     description="A nice way of implementing the Page Object pattern.",
     long_description=read('README.rst'),
     author="Joshua Fehler",
