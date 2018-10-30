44d0cb284849760e8e85cade96b6652f07b9f9c8
Joshua Fehler <jsfehler@gmail.com>
2018-10-30 14:51:01 -0400

    Docs: Cleanup Root and Text info

--- stere/fields/generic/root.py
+++ stere/fields/generic/root.py
@@ -2,6 +2,20 @@ from ..field import Field
 
 
 class Root(Field):
-    """Convenience Class on top of Field."""
+    """A simple wrapper over Field, it does not implement a performer method.
+    Although Root has no specific behaviour, it can be useful when declaring a
+    root for an Area or RepeatingArea.
+
+    Example:
+
+        >>> from stere.areas import RepeatingArea
+        >>> from stere.fields import Root
+        >>>
+        >>>
+        >>> collections = RepeatingArea(
+        >>>     root=Root('xpath', '//table/tr'),
+        >>>     quantity=Text('css', '.collection_qty'),
+        >>> )
+    """
     def find(self):
         return self._element.find()
--- stere/fields/generic/text.py
+++ stere/fields/generic/text.py
@@ -2,5 +2,15 @@ from ..field import Field
 
 
 class Text(Field):
-    """Convenience Class on top of Field."""
+    """A simple wrapper over Field, it does not implement a performer method.
+    Although Root has no specific behaviour, it can be useful when declaring
+    that a Field should just be static Text.
+
+    Example:
+
+        >>> from stere.fields import Text
+        >>>
+        >>>
+        >>> self.price = Text('id', 'item_price')
+    """
     pass
