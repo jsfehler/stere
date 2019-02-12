b3ddd61177c383b43bbfc44f17b7d79f1590db9e
Joshua Fehler <jsfehler@gmail.com>
2019-02-12 10:39:12 -0500

    Fixup

--- tests/browserenabled/test_browser_enabled.py
+++ tests/browserenabled/test_browser_enabled.py
@@ -1,7 +1,7 @@
 from stere.browserenabled import BrowserEnabled
 
 
-def test_browserenabled_browser():
+def test_browserenabled_default_browser():
     """Given stere has not been configured at all
     Then the browser attribute should be None."""
     browser_enabled = BrowserEnabled()
@@ -9,7 +9,7 @@ def test_browserenabled_browser():
     assert browser_enabled.browser is None
 
 
-def test_browserenabled_url_navigator():
+def test_browserenabled_default_url_navigator():
     """Given stere has not been configured at all
     Then the url navigator attribute should be an empty string."""
     browser_enabled = BrowserEnabled()
