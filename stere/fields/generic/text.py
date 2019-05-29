487f02f2d1dd5db1dcf5124a891d688f0a430977
Joshua Fehler <jsfehler@gmail.com>
2019-05-29 10:57:39 -0400

    Fix typo in docstring

--- stere/fields/generic/text.py
+++ stere/fields/generic/text.py
@@ -5,7 +5,7 @@ from ..field import Field
 @stere_performer('null_action', consumes_arg=False)
 class Text(Field):
     """A simple wrapper over Field, it does not implement a performer method.
-    Although Root has no specific behaviour, it can be useful when declaring
+    Although Text has no specific behaviour, it can be useful when declaring
     that a Field should just be static Text.
 
     Example:
