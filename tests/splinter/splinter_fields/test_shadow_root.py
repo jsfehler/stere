718c1f800010d98f373c2a16c13331a976f0ce8e
Joshua Fehler <jsfehler@gmail.com>
2022-06-14 13:36:43 -0400

    fixup

--- tests/splinter/splinter_fields/test_shadow_root.py
+++ tests/splinter/splinter_fields/test_shadow_root.py
@@ -1,3 +1,15 @@
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def skip_by_browser(request, browser_name):
+    marker = request.node.get_closest_marker('skip_if_browser')
+
+    if marker.args[0] == browser_name:
+        pytest.skip(marker.args[1])
+
+
+@pytest.mark.skip_if_browser('firefox', "Can't get shadowRoot in firefox")
 def test_shadow_root_find_all(test_page):
     """When I find the shadow root of an element
     Then the elements in the shadow root can be found.
